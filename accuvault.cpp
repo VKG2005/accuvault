63eea422033550986ff8efc4f71c63138a786508
VK Goswami <143211818+VKG2005@users.noreply.github.com>
2024-08-04 15:25:59 +0530

    Function created for getting account details.

--- accuvault.cpp
+++ accuvault.cpp
@@ -7,13 +7,26 @@ class New_account_opening{
 private: 
 	vector<string> Name;
 	vector<int> Age;
+	vector<string>Aadhar_Number;
+	vector<string>DOB;
+
 public:
-//function for adding the accoungt holder deatail
+
+//function for getting the account holder detail for account opening;
 	void add_details() {
 		string name;
 		int age;
-		cout << "the name of person:" << "\n";
+		string aadhar_number;
+		string dob;
+
+		cout << "The name of person:" << " " << "\n";
 		getline(cin, name);
+		cout << "Enter the age of person:" << " " << "\n";
+		cin >> age;
+		cout << "Enter your aadhar Number:" << " " << "\n";
+		getline(cin, aadhar_number);
+		cout << "Enter your birthdate:" << " " << "\n";
+		getline(cin, dob);
 	}
 
 };
